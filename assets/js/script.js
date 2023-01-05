4e89953a7013d94478ebc8363712faff6b0b1fc7
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-01-05 15:26:08 +0000

    add funcionality to How to play game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-// giving funcionality to "How to play the game" button.
+// Wait for the DOM finishing load before starting the game.
+document.addEventListener("DOMContentLoaded", function () {
+    // hidde info box on DOM Load
+    document.getElementById("rules").style.display = "none";
 
-let info = document.getElementById('info');
-console.log(info);
+    let info = document.getElementById("info");
+    info.addEventListener("click", showInfoBox);
+
+    let close = document.getElementById("closeInfo");
+    close.addEventListener("click", closeInfoBox);
+
+
+
+});
+
+
+/** showes the info & rules box onclick "How to play game?" */
+function showInfoBox() {
+    document.getElementById("rules").style.display = "block";
+}
+
+function closeInfoBox() {
+    document.getElementById("rules").style.display = "none";
+}
