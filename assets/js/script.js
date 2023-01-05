0bbd5bf055accace8b9dd3e74010d6f6372838b3
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-01-05 16:14:25 +0000

    add funcionality to start game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,29 @@ document.addEventListener("DOMContentLoaded", function () {
     let close = document.getElementById("closeInfo");
     close.addEventListener("click", closeInfoBox);
 
+    // hide user details form
+    document.getElementById("user").style.display = "none";
+
+    let user = document.getElementById("start");
+    user.addEventListener("click", getName);
+
 
 
 });
 
 
-/** showes the info & rules box onclick "How to play game?" */
+/** showes the info & rules box after click "How to play game?" */
 function showInfoBox() {
     document.getElementById("rules").style.display = "block";
 }
 
+/** hides the info & rules box after click "Understand!*/
 function closeInfoBox() {
+    document.getElementById("rules").style.display = "none";
+}
+
+/** shows user details form after click "Start Game" */
+function getName() {
+    document.getElementById("user").style.display = "block";
     document.getElementById("rules").style.display = "none";
 }
